66a5e908d017b7e74cfe3f6e6765a7240795ac35
Rokon Khan <rokonjust@gmail.com>
2025-12-10 00:42:13 +0600

    Remove .next folder from repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZXGh2tPw0pHpYAoCoaEvsFSGiEvEAUneGmhyCRlC1pA=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_514302b3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__36b083e1._.css",
-  "static/chunks/node_modules__pnpm_074310ef._.js",
-  "static/chunks/_dba77736._.js"
-],
-    source: "dynamic"
-});
